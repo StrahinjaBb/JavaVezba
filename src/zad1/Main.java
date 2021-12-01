d46e29313795e5e695d370f76ef4d1b6a8cf78fd
StrahinjaBb <straleb123@gmail.com>
2021-12-01 19:22:45 +0100

    Main prvi commit

--- src/zad1/Main.java
+++ src/zad1/Main.java
@@ -1,7 +1,22 @@
 package zad1;
+import java.util.Scanner;
 
 public class Main {
+    private static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
         System.out.println("Pozdrav");
+
+        Brod b1 = new Brod(1, 0, 0, 0);
+        System.out.println(b1.toString());
+        double[] koordinate = new double[3];
+        System.out.println("Unesite koordinate:");
+        for(int i=0; i<koordinate.length; i++)
+            koordinate[i] = sc.nextDouble();
+
+        Brod b2 = new Brod(2, koordinate[0], koordinate[1], koordinate[2]);
+        System.out.println(b2.toString());
+
+        System.out.println("Udaljenost broda " + b1.toString() + " i " + b2.toString() + " je: " + b1.udaljenost(b2));
     }
 }
